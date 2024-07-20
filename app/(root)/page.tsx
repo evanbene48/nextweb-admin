bacfd8f1a78b534dcd6eb8e662ae4be56a4bc95e
evanbene <evanbene48@gmail.com>
2024-07-20 09:32:44 +0700

    clerk authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <div className="p-4">
-      <Button variant="destructive">Hello</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
